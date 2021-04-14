99dcb23ffac174ca29be3e7db73e5c7ef05ba655
André Gomes da Silva <67976938+andregds85@users.noreply.github.com>
2021-04-14 16:30:33 -0300

    teste
    teste

--- index.php
+++ index.php
@@ -70,17 +70,16 @@
       </script>	
 </head>
 <body>
-  <nav class="light-blue lighten-1" role="navigation">
+  <nav class="#1b5e20 green darken-4" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"><img src="img/logo_menor.png">Cartão Nacional</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="http://www.coronavirus.sc.gov.br/">Site do Coronavirus</a></li>
+        <li><a href="#">Inicio</a></li>
         <li><a href="http://importacartao.eletivassc.com.br">Importa Cartão</a></li>
         <li><a class="white-text" href="https://www.saude.sc.gov.br/">SES</a></li>
 
       </ul>
 
       <ul id="nav-mobile" class="sidenav">
-        <li><a href="http://www.coronavirus.sc.gov.br/">Site do CoronaVirus</a></li>
         <li><a href="http://importacartao.eletivassc.com.br">Importa Cartão</a></li>
         <li><a href="https://www.saude.sc.gov.br/">SES</a></li>
 
@@ -92,13 +91,14 @@
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
-      <h1 class="header center orange-text">Consulta Paciente</h1>
+      <h1 class="header center blue-text">Consulta Paciente</h1>
       <div class="row center">
-        <h5 class="header col s12 light">Verifique o paciente digitando o cartão nacional abaixo</h5>
+    
+
+      <h5 class="yeloow-text">Verifique o paciente digitando o cartão nacional abaixo</h5>
       </div>
       <div class="row center">
-      
-      
+        
              
 <?php include("contador.php"); ?>  
 
@@ -115,8 +115,7 @@
       </fieldset>
     </form>
 
-          
-        
+       
 
         <?php 
 
@@ -125,12 +124,7 @@
 
        ?>
       
-    
-    
-    
-    
-    
-    
+        
     
       <div id="contentLoading">
 				<div id="loading"></div>
@@ -163,7 +157,6 @@
         <div class="col s12 m4">
           <div class="icon-block">
 
-            <img class="responsive-img" src="img/corona.jpg">
           </div>
         </div>
 
@@ -179,7 +172,9 @@
     <br><br>
   </div>
 
-  <footer class="page-footer orange">
+ 
+
+  <footer class="#1b5e20 green darken-4">
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
@@ -197,14 +192,13 @@
           <ul>
             <li><a class="white-text" href="https://www.saude.sc.gov.br/">SES</a></li>
             <li><a class="white-text" href="http://importacartao.eletivassc.com.br/">Importa Cartão</a></li>
-            <li><a class="white-text" href="http://www.coronavirus.sc.gov.br/">Site do Coronavirus</a></li>
           </ul>
         </div>
       </div>
     </div>
     <div class="footer-copyright">
-      <div class="container">Secretaria de Estado da Saúde 
-    @ <?php echo $today; ?> <a class="orange-text text-lighten-3" href="http://materializecss.com"></a>
+      <div class="container"><h6 class="header center blue-text">Secretaria de Estado da Saúde 
+    @ <?php echo $today; ?> <a class="orange-text text-lighten-3" href="http://materializecss.com"></a></h6>
       </div>
     </div>
   </footer>
